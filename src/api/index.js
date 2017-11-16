262acabf5b6b50859b73b6d3cbdbfec48727375e
Eduardo M(bbaaxx) <bbaaxx@gmail.com>
2017-11-15 23:48:25 -0600

    prettied all js files and added linters

--- src/api/index.js
+++ src/api/index.js
@@ -4,17 +4,17 @@ import facets from './facets';
 import characters from './characters';
 
 export default ({ config, db }) => {
-	let api = Router();
+  let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+  // mount the facets resource
+  api.use('/facets', facets({ config, db }));
 
-	api.use('/characters', characters({ config, db }));
+  api.use('/characters', characters({ config, db }));
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+  // perhaps expose some API metadata at the root
+  api.get('/', (req, res) => {
+    res.json({ version });
+  });
 
-	return api;
-}
+  return api;
+};
